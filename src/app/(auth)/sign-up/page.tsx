eb0e5011b9296875aabcd079ec805ecc8a0c54a7
Julian Rivera <julianarevircoins@gmail.com>
2025-09-28 16:31:42 -0400

    05-auth-social

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,16 @@
 import { SignUpView } from "@/modules/auth/ui/views/sign-up-view"
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+const Page = async() => {
 
-const Page = () => {
+    const session = await auth.api.getSession({
+      headers:await headers(),
+    })
+  
+    if(!!session){
+      redirect("/");
+    }
   return <SignUpView/>
 }
 
