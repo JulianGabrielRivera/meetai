8a372242c259ada57a4f91a4aca9e2006330d943
Julian Rivera <julianarevircoins@gmail.com>
2025-09-30 01:43:35 -0400

    08-trpc-setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { authClient } from "@/lib/auth-client";
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () =>{
-    const router = useRouter();
+    const trpc = useTRPC();
+    const {data} = useQuery(trpc.hello.queryOptions({text:"Julian"}));
   const {data:session} =authClient.useSession();
   
   if(!session){
@@ -16,8 +17,7 @@ export const HomeView = () =>{
   
   return (
    <div className="flex flex-col p-4 gap-y-4">
-        <p>Logged in as {session.user.name}</p>
-        <Button onClick={()=>authClient.signOut({fetchOptions: {onSuccess: () => router.push("/sign-in")}})}>Sign Out</Button>
+       {data?.greeting}
       </div>
   );
 }
